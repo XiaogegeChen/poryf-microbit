9ee09b1e5f947718bb470578e5d15150e8638212
zhengzhengxiaogege <39449547+zhengzhengxiaogege@users.noreply.github.com>
2018-08-23 23:54:54 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,35 @@
 
 //% color="#808080" weight=23 icon="\uf11c"
 namespace pbit_传感器 {
-    export enum enLED1 {
-        
+    
+    
+ /*单色灯亮灭*/
+    export enum enLED1 { 
         //% blockId="OFF" block="灭"
         OFF = 0,
         //% blockId="ON" block="亮"
         ON =1
     }
-    //% blockId=mbit_LED1 block="LED灯|管脚 %pin|状态 %value"
+    //% blockId=mbit_LED1 block="LED灯 管脚 %pin|状态 %value"
     //% weight=5
     //% blockGap=8
     //% color="#C814B8"
     export function LED1(pin: DigitalPin, value: enLED1): void {
-
         pins.digitalWritePin(pin, value);
-
     }
-
+    
+    
+   /*单色灯亮度调节*/ 
+   //% blockId=mbit_LED2 block="LED灯 管脚 %pin|亮度（0~255） %value"
+    //% weight=4
+    //% blockGap=8
+    //% color="#C814B8"
+    //% value.min=0 value.max=255
+    export function LED2(pin: AnalogPin, value: number): void {
+        pins.analogWritePin(pin, value * 1024 / 256);
+    } 
+    
+    /*按键*/ 
     export enum enRocker {
         //% blockId="Nostate" block="无"
         Nostate = 0,
