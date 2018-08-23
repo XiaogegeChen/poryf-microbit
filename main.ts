9a2693f0729214cf5d451e4f67db54e80de98901
zhengzhengxiaogege <39449547+zhengzhengxiaogege@users.noreply.github.com>
2018-08-24 00:52:23 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -73,4 +73,88 @@ namespace pbit_传感器 {
     }
            
            
+      /*水滴传感器*/
+       export enum enWater {
+        //% blockId="NoWater" block="无水滴"
+        NoWater = 0,
+        //% blockId="Water" block="有水滴"
+        Watere = 1
+    //% blockId=mbit_Fire_Sensor block="水滴传感器 管脚 %pin|状态 %value"
+    //% weight=4
+    //% blockGap=8
+    //% color="#C814B8"
+           export function Water_Sensor(pin: DigitalPin, value: enWater): boolean {
+        pins.setPull(pin, PinPullMode.PullUp);
+        if (pins.digitalReadPin(pin) == value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    } 
+           
+           
+     /*磁感应传感器*/
+       export enum enMagnet {
+        //% blockId="Magnet" block="有磁铁"
+        Magnet = 0,
+        //% blockId="NoMagnet" block="无磁铁"
+        NoMagnet = 1
+    //% blockId=mbit_Magnet_Sensor block="磁感应传感器 管脚 %pin|状态 %value"
+    //% weight=4
+    //% blockGap=8
+    //% color="#C814B8"
+           export function Magnet_Sensor(pin: DigitalPin, value: enMagnet): boolean {
+        pins.setPull(pin, PinPullMode.PullUp);
+        if (pins.digitalReadPin(pin) == value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    } 
+           
+           
+      /*人体红外传感器*/
+       export enum enHumanInfrared {
+        //% blockId="HumanInfrared" block="有人"
+        HumanInfrared = 1,
+        //% blockId="NoHumanInfrared" block="无人"
+        NoHumanInfrared = 0
+    //% blockId=mbit_HumanInfrared_Sensor block="磁感应传感器 管脚 %pin|状态 %value"
+    //% weight=4
+    //% blockGap=8
+    //% color="#C814B8"
+           export function HumanInfrared_Sensor(pin: DigitalPin, value: enHumanInfrared): boolean {
+        pins.setPull(pin, PinPullMode.PullUp);
+        if (pins.digitalReadPin(pin) == value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    } 
+           
+           
+     /*红外循迹传感器*/
+       export enum enInfraredTracking {
+        //% blockId="InfraredTracking" block="检测到黑色"
+        InfraredTracking = 0,
+        //% blockId="NoInfraredTracking" block="未检测到黑色"
+        NoInfraredTracking = 1
+    //% blockId=mbit_InfraredTracking_Sensor block="磁感应传感器 管脚 %pin|状态 %value"
+    //% weight=4
+    //% blockGap=8
+    //% color="#C814B8"
+           export function InfraredTracking_Sensor(pin: DigitalPin, value: enInfraredTracking): boolean {
+        pins.setPull(pin, PinPullMode.PullUp);
+        if (pins.digitalReadPin(pin) == value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    } 
+           
+           
            
