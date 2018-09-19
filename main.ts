c3d00293030da92dbd437db9add8a541f91eb180
zhengzhengxiaogege <39449547+zhengzhengxiaogege@users.noreply.github.com>
2018-09-19 17:04:59 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,58 +1,135 @@
-//% color="#87CEEB" weight=100 icon="\uf1b6"
-namespace DHT11 {
-
-    let pin = DigitalPin.P1;
-    let item: number[] = [];
-    item = [0, 0, 0, 0, 0];
-    //% color="#87CEEB" weight=100 blockGap=8
-    //% blockId=dht11_set_pin block="温湿度传感器(DHT11) 管脚号 %dht11_pin"
-    export function set_pin(dht11_pin: DigitalPin): void {
-        pin = dht11_pin;
+//%weight=106 color="#8B008B" icon="\uF610"
+namespace LCD12846 {
+    let LCD12864_CS = DigitalPin.P13;
+    let LCD12864_SID = DigitalPin.P14;
+    let LCD12864_SCK = DigitalPin.P15;
+    let addr_table = [0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
+                      0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97,
+                      0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f,
+                      0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f];
+     
+    /*********选择引脚号（默认CS是13号引脚，SID是14号引脚，SCK是15号引脚）********/
+    //%blockId=LCD12864_pins_Actuator block="设置LCD12864引脚 4号引脚 %pin1|5号引脚 %pin2|6号引脚 %pin3"
+    //%weight=100 blockGap=10 color="#87CEEB"
+    export function LCD12864_init(pin1: DigitalPin, pin2: DigitalPin, pin3: DigitalPin): void {
+        LCD12864_CS = pin1;
+        LCD12864_SID = pin2;
+        LCD12864_SCK = pin3;
+        LCD12864_lcmClear();
     }
-    function dht11_get(): void {
-        let cnt = 7;
-        let idx = 0;
-        let t = 0;
-        for (let i = 0; i < 5; i++) {
-            item[i] = 0;
+
+    //发送一个字节的数据
+    function LCD12864_sendByte(dat: number): void {
+        for (let i = 0; i < 8;i++) {
+            LCD12864_SCK = 0;
+            if (dat & 0x80) {
+                LCD12864_SID = 1;
+            } else {
+                LCD12864_SID = 0;
+            }
+            dat = dat << 1;
+            LCD12864_SCK = 1;
+            LCD12864_SCK = 0;
         }
-        pins.digitalWritePin(pin, 0);
-        basic.pause(18);
-        pins.digitalWritePin(pin, 1);
-        t += 1;
-        t += 1;
-        t += 1;
-        pins.setPull(pin, PinPullMode.PullUp);
-        while (pins.digitalReadPin(pin) == 1); 
-        while (pins.digitalReadPin(pin) == 0);
-        while (pins.digitalReadPin(pin) == 1);
-        for (let j = 0; j < 40; j++) {
-            let count = 0;
-            while (pins.digitalReadPin(pin) == 0);
-            while (pins.digitalReadPin(pin) == 1) {
-                count += 1;
-                if (count > 4) {
-                    item[idx] = item[idx] + (1 << cnt);
-                }
-                if (cnt == 0) {
-                    cnt = 7;
-                    idx++;
-                } else {
-                    cnt--;
-                }
+    }
+
+    //接收一个字节的数据
+    function LCD12864_receiveByte(): number {
+        let temp1 = 0;
+        let temp2 = 0;
+        for (let i = 0; i < 8; i++) {
+            temp1 = temp1 << 1;
+            LCD12864_SCK = 0;
+            LCD12864_SCK = 1;
+            LCD12864_SCK = 0;
+            if (LCD12864_SID) {
+                temp1++;
             }
         }
+        for (let i = 0; i < 8; i++) {
+            temp2= temp2 << 1;
+            LCD12864_SCK = 0;
+            LCD12864_SCK = 1;
+            LCD12864_SCK = 0;
+            if (LCD12864_SID) {
+                temp2++;
+            }
+        }
+        return ((0xf0 & temp1) + (0x0f & temp2));
     }
-    //% color="#87CEEB" weight=98 blockGap=8
-    //%blockId=dht11_getTemperature block="获得温度值(DHT11)"
-    export function getTemperature(): number {
-        dht11_get();
-        return item[2];
+
+    //判忙
+    function LCD12864_checkBusy(): void {
+        do LCD12864_sendByte(0xfc);
+        while (0x80 & LCD12864_receiveByte());
     }
-    //% color="#87CEEB" weight=96 blockGap=8
-    //%blockId=dht11_getHumidity block="获得湿度值(DHT11)"
-    export function getHumidity(): number {
-        dht11_get();
-        return item[0];
+
+    //写命令
+    function LCD12864_writeCommand(cmd: number): void {
+        LCD12864_CS = 1;
+        LCD12864_checkBusy();
+        LCD12864_sendByte(0xf8);
+        LCD12864_sendByte(0xf0 & cmd);
+        LCD12864_sendByte(0xf0 & (cmd << 4));
+        LCD12864_CS = 0;
+    }
+
+    //写数据
+    function LCD12864_writeData(data:number): void {
+        LCD12864_CS = 1;
+        LCD12864_checkBusy();
+        LCD12864_sendByte(0xfa);
+        LCD12864_sendByte(0xf0 & data);
+        LCD12864_sendByte(0xf0 & (data << 4));
+        LCD12864_CS = 0;
+    }
+
+    //读数据
+    function LCD12864_readData(): number {
+        LCD12864_checkBusy();
+        LCD12864_sendByte(0xfe);
+        return LCD12864_receiveByte();
+    }
+
+    //初始化显示屏
+    function LCD12864_lcmInit(): void {
+        LCD12864_writeCommand(0x30);
+        LCD12864_writeCommand(0x03);
+        LCD12864_writeCommand(0x0c);
+        LCD12864_writeCommand(0x01);
+        LCD12864_writeCommand(0x06);
+    }
+
+    /*********清屏********/
+    //%blockId=LCD12864_lcmClear block="清除所有显示内容"
+    //%weight=96 blockGap=10 color="#87CEEB"
+    function LCD12864_lcmClear(): void {
+        LCD12864_writeCommand(0x30);
+        LCD12864_writeCommand(0x80);
+        for (let i = 0; i < 64;i++) {
+            LCD12864_writeData(0x20);
+        }
+    }
+
+    /*********指定位置显示汉字********/
+    //%blockId=LCD12864_showString_en block="在第%row行第%col列显示汉字%Str"
+    //%weight=92 blockGap=10 color="#87CEEB"
+    function LCD12864_showString_en(row: number, col: number, Str: string): void {
+        basic.pause(100);
+        LCD12864_lcmInit();
+        LCD12864_writeCommand(0x30);
+        LCD12864_writeCommand(addr_table[(row - 1) * 8 + (col - 1)]);
+        for (let i = 0; i < Str.length;i++) {
+            if (col==9) {
+                col = 1;
+                row++;
+            }
+            if (row==5) {
+                row = 1;
+            }
+            LCD12864_writeCommand(addr_table[(row - 1) * 8 + (col - 1)]);
+            LCD12864_writeData(Str.charCodeAt(i));
+            col++;
+        }
     }
 }
